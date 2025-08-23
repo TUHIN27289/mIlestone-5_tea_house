bf5bf3bf4eb4e15bc9eb9976662cce4ae078f6f7
Toukir <toukerahmed.2728@gmail.com>
2025-08-23 23:14:32 +0600

    24-6 Styling dom properties, add and remove CSS class using JS

--- js/avi.js
+++ js/avi.js
@@ -1,2 +1,16 @@
 console.log('this is external jssssssssssssss');
 console.log(document);
+
+const si_=document.querySelectorAll('section');
+// for(const i of si_){
+//     i.style.border='2px solid red';
+//     i.style.marginBottom='10px';
+//     i.style.paddingLeft='10px';
+//     i.style.backgroundColor='lightgray';
+//     i.style.borderRadius='10px';
+
+// }
+
+for(const section of si_){
+    section.classList.add('section-card')
+}
